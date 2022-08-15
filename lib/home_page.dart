12d19653f138e14ded28f9c3b17bcd6a1b781db0
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-08-15 19:46:36 +0530

    some changes no big addition on click card the four option show up

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,17 +12,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool ontapp = false;
+  bool istapOnBox = false;
   final inputFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     double width = size.width;
+
     // double height = size.height;
     return GestureDetector(
       onTap: () {
-        setState(() {
-          inputFocusNode.unfocus();
-        });
+        setState(
+          () {
+            inputFocusNode.unfocus();
+          },
+        );
         log("vujbi");
       },
       child: Scaffold(
@@ -34,9 +38,10 @@ class _HomePageState extends State<HomePage> {
               const Text(
                 "Diskit",
                 style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'FredokaOne',
-                    color: Color.fromARGB(255, 118, 57, 13)),
+                  fontSize: 30,
+                  fontFamily: 'FredokaOne',
+                  color: Color.fromARGB(255, 171, 171, 171),
+                ),
               ),
               Container(
                 margin: const EdgeInsets.symmetric(vertical: 10),
@@ -55,110 +60,125 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         body: Center(
-          child: ListView(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: ((context, i) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    istapOnBox = !istapOnBox;
+                  });
+                },
                 child: Container(
-                  padding: const EdgeInsets.symmetric(
-                      // vertical: 10,
-                      // horizontal: 10,
-                      ),
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        // mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 10),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.asset(
-                                'assets/images/profile.jpg',
-                                scale: 10,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 0.04 * width,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        // vertical: 10,
+                        // horizontal: 10,
+                        ),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          // mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  'assets/images/profile.jpg',
+                                  scale: 10,
+                                ),
                               ),
                             ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.all(10),
-                            width: 0.5 * width,
-                            child: Column(
-                              children: [
-                                RichText(
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 4,
-                                  text: const TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text:
-                                            "Quick and easy custom thumbnail\n",
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
+                            Container(
+                              margin: const EdgeInsets.all(10),
+                              width: 0.45 * width,
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 50,
+                                    child: RichText(
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                      text: const TextSpan(
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                "Quick and easy custom thumbnail\n",
+                                            style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ],
                                       ),
+                                    ),
+                                  ),
+                                  Row(
+                                    children: const [
+                                      Icon(Icons.online_prediction),
+                                      Text("344 in past 24hrs"),
                                     ],
                                   ),
-                                ),
-                                Row(
-                                  children: const [
-                                    Icon(Icons.online_prediction),
-                                    Text("344"),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      Container(
-                        width: width,
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 13,
-                          horizontal: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.vertical(
-                              top: Radius.circular(0),
-                              bottom: Radius.circular(10)),
-                          color: Colors.white.withOpacity(0.5),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Row(
-                              children: const [
-                                Icon(Icons.thumb_up),
-                                Text(" 0.2K"),
-                              ],
-                            ),
-                            Row(
-                              children: const [
-                                Icon(Icons.person),
-                                Text("2.5K"),
-                              ],
-                            ),
-                            const Icon(
-                              Icons.chat_bubble,
-                              color: Color.fromARGB(255, 133, 173, 84),
-                            ),
-                            const Icon(Icons.share),
+                                ],
+                              ),
+                            )
                           ],
                         ),
-                      )
-                    ],
+                        istapOnBox
+                            ? Container(
+                                width: width,
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 13,
+                                  horizontal: 10,
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.vertical(
+                                      top: Radius.circular(0),
+                                      bottom: Radius.circular(5)),
+                                  color: Colors.white.withOpacity(0.5),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Row(
+                                      children: const [
+                                        Icon(Icons.thumb_up),
+                                        Text(" 0.2K"),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: const [
+                                        Icon(Icons.person),
+                                        Text("2.5K"),
+                                      ],
+                                    ),
+                                    const Icon(
+                                      Icons.chat_bubble,
+                                      color: Color.fromARGB(255, 133, 173, 84),
+                                    ),
+                                    const Icon(Icons.share),
+                                  ],
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+              );
+            }),
           ),
         ),
         floatingActionButton: inputFocusNode.hasFocus
